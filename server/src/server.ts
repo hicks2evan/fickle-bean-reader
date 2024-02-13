7f32d7620cc49982fc47789995c06fd40f2b2c08
Evan Hicks <hicks2evan@gmail.com>
2024-02-13 15:07:43 -0700

    Server Hello World

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from 'express';
+const app = express()
+const port = process.env.PORT;
+
+app.get('/', (_: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
