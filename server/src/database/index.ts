7021db1cf65d23b7972e20cd62a4ec9b7a41c4c5
“Evan <hicks2evan@gmail.com>
2024-02-14 14:37:33 -0700

    Remove log

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    console.log(process.env.DATABASE_URI);
     await mongoose.connect(process.env.DATABASE_URI || "");
     console.log('Successfully connected to database');
   } catch (error) {
