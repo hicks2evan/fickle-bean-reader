d7301eac4ce5a859ec2c88b347082bda741e6b30
“Evan <hicks2evan@gmail.com>
2024-02-14 14:40:24 -0700

    Update database connection to default to prod uri if available

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    await mongoose.connect(process.env.DATABASE_URI || "");
+    await mongoose.connect(process.env.DATABASE_CONNECTION_URI_PROD || process.env.DATABASE_URI || "");
     console.log('Successfully connected to database');
   } catch (error) {
     console.error(error);
