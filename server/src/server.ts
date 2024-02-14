5c5c72ca54235c392b9a5c95d2eac75060ff11dd
“Evan <hicks2evan@gmail.com>
2024-02-14 10:16:50 -0700

    Add deploy for server, small change to test

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT;
 
 app.get('/', (_: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('Hello, Heroku?')
 })
 
 app.listen(port, () => {
