cf11458b627806f66686e6d9196856370aa419f1
“Evan <hicks2evan@gmail.com>
2024-02-15 21:24:14 -0700

    Undo logging

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -2,9 +2,6 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    console.log("NODE_ENV:" + process.env.NODE_ENV);
-    console.log("DATABASE_CONNECTION_URI_PROD:" + process.env.DATABASE_CONNECTION_URI_PROD);
-    console.log("DATABASE_URI:" + process.env.DATABASE_URI);
     await mongoose.connect(process.env.NODE_ENV === 'production' ? (process.env.DATABASE_CONNECTION_URI_PROD || "") : (process.env.DATABASE_URI || ""));
     console.log('Successfully connected to database');
   } catch (error) {
